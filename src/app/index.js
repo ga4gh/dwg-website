78dccf9cc12950edf7b563bba71a93b12ae9d01b
Stephen Keenan <keenan@ebi.ac.uk>
2015-09-16 16:55:22 +0100

    switched to ui.router added use cases stub

--- src/app/index.js
+++ src/app/index.js
@@ -1,92 +1,129 @@
 'use strict';
 
-var app = angular.module('ga4gh', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngRoute', 'ngMaterial', 'times.tabletop', 'hc.marked', 'ngMdIcons', 'uiGmapgoogle-maps']).config(function ($routeProvider, $mdThemingProvider, $mdIconProvider, TabletopProvider) {
+var app = angular.module('ga4gh', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ui.router', 'ngMaterial', 'times.tabletop', 'hc.marked', 'ngMdIcons', 'uiGmapgoogle-maps']).config(function($stateProvider, $urlRouterProvider, $mdThemingProvider, $mdIconProvider, TabletopProvider, markedProvider, uiGmapGoogleMapApiProvider) {
 
 	TabletopProvider.setTabletopOptions({
 		key: 'https://docs.google.com/spreadsheets/d/1NPM-d8ER7sbDjWmNwOW6kQuXWEtXOv4bq3cN0yhGVWw/pubhtml',
-        simpleSheet: false
+		simpleSheet: false
 	});
 
-	$routeProvider.when('/', {
+	$urlRouterProvider.otherwise("/");
+
+	// $stateProvider.state('main', {
+	// 	url: "/",
+	// 	templateUrl: "app/main/main.html",
+        // controller: 'MainCtrl'
+	// }).state('state1.list', {
+	// 	url: "/list",
+	// 	templateUrl: "partials/state1.list.html",
+	// 	controller: function($scope) {
+	// 		$scope.items = ["A", "List", "Of", "Items"];
+	// 	}
+	// }).state('state2', {
+	// 	url: "/state2",
+	// 	templateUrl: "partials/state2.html"
+	// }).state('state2.list', {
+	// 	url: "/list",
+	// 	templateUrl: "partials/state2.list.html",
+	// 	controller: function($scope) {
+	// 		$scope.things = ["A", "Set", "Of", "Things"];
+	// 	}
+	// });
+
+	$stateProvider.state('main', {
+        url: '/',
 		templateUrl: 'app/main/main.html',
 		controller: 'MainCtrl'
 	}).
-		when('/metadata-team', {
+		state('metadata-team', {
+            url: '/metadata-team',
 			templateUrl: 'app/metadata_team/metadata_team.html',
 			controller: 'MetadataTeamCtrl'
 		}).
-		when('/g2p-team', {
+		state('g2p-team', {
+            url: '/g2p-team',
 			templateUrl: 'app/g2p_team/g2p_team.html',
 			controller: 'G2PTeamCtrl'
 		}).
-		when('/refvar-team', {
+		state('refvar-team', {
+            url: '/refvar-team',
 			templateUrl: 'app/refvar_team/refvar_team.html',
 			controller: 'RefVarTeamCtrl'
 		}).
-		when('/cwf-team', {
+		state('cwf-team', {
+            url: '/cwf-team',
 			templateUrl: 'app/cwf_team/cwf_team.html',
 			controller: 'CWFTeamCtrl'
 		}).
-		when('/benchmarking-team', {
+		state('benchmarking-team', {
+            url: '/benchmarking-team',
 			templateUrl: 'app/benchmarking_team/benchmarking_team.html',
 			controller: 'BenchmarkingTeamCtrl'
 		}).
-		when('/variantannotation-team', {
+		state('variantannotation-team', {
+            url: '/variantannotation-team',
 			templateUrl: 'app/variantannotation_team/variantannotation_team.html',
 			controller: 'VariantAnnotationTeamCtrl'
 		}).
-		when('/fileformats-team', {
+		state('fileformats-team', {
+            url: '/fileformats-team',
 			templateUrl: 'app/fileformats_team/fileformats_team.html',
 			controller: 'FileFormatsTeamCtrl'
 		}).
-		when('/reads-team', {
+		state('reads-team', {
+            url: '/reads-team',
 			templateUrl: 'app/reads_team/reads_team.html',
 			controller: 'ReadsTeamCtrl'
 		}).
-		when('/rnaseq-team', {
+		state('rnaseq-team', {
+            url: '/rnaseq-team',
 			templateUrl: 'app/rnaseq_team/rnaseq_team.html',
 			controller: 'RNAseqTeamCtrl'
 		}).
-		when('/beacon', {
+		state('beacon', {
+            url: '/beacon',
 			templateUrl: 'app/beacon_team/beacon_team.html',
 			controller: 'BeaconTeamCtrl'
 		}).
-		when('/mme', {
+		state('/mme', {
+            url: '/mme',
 			templateUrl: 'app/mme/mme.html',
 			controller: 'mmeCtrl'
 		}).
-		when('/oct2015meeting', {
+		state('oct2015meeting', {
+            url: '/oct2015meeting',
 			templateUrl: 'app/oct2015meeting/oct2015meeting.html',
 			controller: 'octCtrl'
 		}).
-		when('/schemas', {
+		state('schemas', {
+            url: '/schemas',
 			templateUrl: 'app/schemas/schemas.html',
 			controller: 'schemasCtrl'
 		}).
-		when('/documentation', {
+		state('documentation', {
+            url: '/documentation',
 			templateUrl: 'app/docs/docs.html',
 			controller: 'docsCtrl'
 		}).
-		when('/api/v0.5.1', {
+		state('api', {
+            url: '/api/v0.5.1',
 			templateUrl: 'app/v051_api/ga4gh_api.html',
 			controller: 'avrodocCtrl'
-		})
-		.otherwise({
-			redirectTo: '/'
+		}).
+		state('usecases', {
+            url: '/usecases',
+			templateUrl: 'app/use_cases/use_cases.html',
 		});
-
 	$mdThemingProvider.theme('default').primaryPalette('indigo').accentPalette('pink');
+	markedProvider.setOptions({
+		gfm: true
+	});
+	uiGmapGoogleMapApiProvider.configure({
+		//    key: 'your api key',
+		v: '3.17',
+		// libraries: 'weather,geometry,visualization'
+	});
 });
 
-app.config(['markedProvider', function (markedProvider) {
-	markedProvider.setOptions({ gfm: true });
-}]);
 
 
-app.config(function (uiGmapGoogleMapApiProvider) {
-    uiGmapGoogleMapApiProvider.configure({
-        //    key: 'your api key',
-        v: '3.17',
-        // libraries: 'weather,geometry,visualization'
-    });
-});
